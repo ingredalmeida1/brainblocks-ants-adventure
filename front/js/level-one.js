f408fdb0a25844a6a03b9ede072c2cb0d52896d2
ingred <ingred.almeida@ufv.br>
2024-02-26 20:56:48 -0300

    add: funcionalidade de movimentar a formiga

--- front/js/level-one.js
+++ front/js/level-one.js
@@ -1,24 +1,91 @@
+var intervalId;
+var currentIndex = 0;
+var moves = [];
+var ant = document.getElementById('ant1');
+var turnFlag = false;
+
+ant.style.left = '140px'; // 140px é a pos inicial
+ant.style.top = '75px'; // 75px é a pos inicial
+
+// DULICAR BLOCO DE CÓDIGO NA COLUNA DE CÓDIGO
 function duplicateCard(id) {
     var originalCard = document.getElementById(id);
     var clonedCard = originalCard.cloneNode(true);
 
-    if (id === 'card1' || id === 'card2') {
+    if (id === 'inicio' || id === 'fim') {
         clonedCard.classList.remove('item1');
         clonedCard.classList.add('cloned-item1');
         document.getElementById('column1').prepend(clonedCard);
-    } else if(id === 'card3') {
+    } else if(id === 'avancar') {
+        moves.push(id);
         clonedCard.classList.remove('item2');
         clonedCard.classList.add('cloned-item2');
         document.getElementById('column1').prepend(clonedCard);
     } else {
-        clonedCard.classList.remove('item3');
+        moves.push(id);
+        clonedCard.classList.remove('virar');
         clonedCard.classList.add('cloned-item3');
         document.getElementById('column1').prepend(clonedCard);
     }
-    
     document.getElementById('column1').appendChild(clonedCard);
 }
 
+// MOVER A FORMIGA DE ACORDO COM A SEQUÊNCIA DE COMANDOS
+function moveAnt(moves) {
+    var currentPositionX = parseInt(ant.style.left) || 0;
+    var currentPositionY = parseInt(ant.style.top) || 0;
+
+    if (moves[currentIndex] === 'avancar') {
+        if (turnFlag) {
+            console.log(turnFlag);
+            console.log('add em y');
+            currentPositionY += 65;
+        } else {
+            console.log(turnFlag);
+            console.log('add em x');
+            currentPositionX += 60;
+        }
+    } else if (moves[currentIndex] === 'virar') {
+        console.log(turnFlag);
+        console.log('virei e add em y');
+        currentPositionY += 65
+        turnFlag = true;
+    }
+
+    if (currentPositionX >= 500 || currentPositionY >= 500 || currentIndex >= moves.length) {
+        stopAutoMove();
+        return;
+    }
+
+    ant.style.left = currentPositionX + 'px';
+    ant.style.top = currentPositionY + 'px';
+
+    currentIndex++;
+}
+
+// MOVER A FORMIGA AO CLICAR NO BOTÃO MOVIMENTAR
+document.getElementById('movimentar').addEventListener('click', function() {
+    startAutoMove(moves); 
+});
+
+function startAutoMove(moves) {
+    currentIndex = 0; 
+    intervalId = setInterval(function() {
+        moveAnt(moves);
+    }, 1000); 
+}
+
+// FINAL DA MOVIMENTAÇÃO
+function stopAutoMove() {
+    ant.style.left = '140px';
+    ant.style.top = '75px';
+    moves = [];
+    var column1 = document.getElementById('column1');
+    column1.innerHTML = '';
+    clearInterval(intervalId);
+}
+
+// LIMPAR COLUNA DE CÓDIGO
 function clearCode() {
     var column1 = document.getElementById('column1');
 
@@ -27,6 +94,7 @@ function clearCode() {
     }
 }
 
+// RETORNAR PARA PÁGINA INICIAL
 function returnToHome() {
     window.location.href = "index.html";
 }
